110661aae3c293ffa896c268887777f128ccc61e
Matthew Pateman <matthewpateman@me.com>
2012-12-18 18:21:16 +0000

    Completed

--- 01_lesson_1_recap.rb
+++ 01_lesson_1_recap.rb
@@ -1,20 +1,48 @@
 # 1. declare two variables and assign them odd integer values
+puts "+++ EXERCISE 1 +++"
+val1 = 79
+val2 = 3
 
+puts val1
+puts val2
 # 2. write an expression dividing the Task #1 variables, but
 #    displaying the full fractional remainder
 
+puts "+++ EXERCISE 2 +++"
+answer = val1.to_f / val2
+puts answer
+
 # 3. use ** and + in a single expression to display the result 21
+
+puts "+++ EXERCISE 3 +++"
+puts 2**4 + 5
 
 # 4. write four expressions, two displaying even numbers modulus 2
 #    and two displaying odd numbers modules 2. notice a pattern?
+
+puts "+++ EXERCISE 4 +++"
+puts 11%3
+puts 17%5
+puts 23%7
 
 # 5. Declare a variable called name with your name and a variable
 #    called age with a number. Display them using concatenation
 #    (using +).
 
+puts "+++ EXERCISE 5 +++"
+name = "Matthew Pateman"
+age = 28
+
+puts name + " is " + age.to_s + " years old."
+
 # 6. display the same output as above, but this time using string
 #    interpolation.
 
+puts "+++ EXERCISE 6 +++"
+puts "#{name} is #{age} years old."
 # 7. do the same as above, but output your name in uppercase. Use
 #    ruby-doc.org or "string".methods in IRB to find a method that
 #    will do this for you.
+
+puts "+++ EXERCISE 7 +++"
+puts "#{name.upcase} is #{age} years old."
--- 02_input_and_conditions.rb
+++ 02_input_and_conditions.rb
@@ -2,6 +2,11 @@
 #    age. Assign both of these to variables. Display the name of the
 #    user and the year they were born in.
 
+puts "What is your name?"
+name = gets.chomp
+puts "What is your age?"
+age = gets.chomp
+puts "#{name} was born in #{2012 - age.to_i}"
 
 
 # 2. Use an if statement to output whether a user has a long or short name,
@@ -11,9 +16,38 @@
 #    letters or longer, display "You have a long name". Otherwise,
 #    display "Your name is average sized".
 
-
+if name.size <= 3
+	puts "You have a short name"
+elsif name.size >= 7
+	puts "You have a long name"
+else
+	puts "Your name is average sized"
+end
 
 # 3. Ask the user to input a degree value from 0 to 359. Assuming 0 is
 #    North, use "case" and "when" to output whether the degrees point
 #    Northeast, Southeast, Southwest, or Northwest. Remember that
 #    gets.chomp retrieves a string value.
+
+puts "Please enter a degree value from 0 to 359:"
+degree = gets.chomp
+
+if degree == nil
+	puts "I guess you are on a different planet"
+else
+	degree = degree.to_i
+
+case degree
+when 0
+	puts "You are north"
+when 1..90
+	puts "You are somewhere north"
+when 91..180
+	puts "You are somewhere east"
+when nil
+	puts "I guess you are on a different planet"
+
+	puts "Dont know where you are. You are using Apple Maps"
+end
+
+end
